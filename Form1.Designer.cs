f86c6d37ce6bb3cd446d234e956d373fe1cc4546
Mohamad khorsandi <severanceline@gmail.com>
2025-04-08 11:59:03 +0330

    c

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -81,7 +81,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(77, 55);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Cr";
+            this.button3.Text = "C";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
